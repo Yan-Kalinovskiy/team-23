267e673aaea5061d7d7ec91f4080b2863cf7ef13
Yan-Kalinovskiy <112379732+Yan-Kalinovskiy@users.noreply.github.com>
2022-11-20 19:20:23 +0000

    Deploying to gh-pages from @ Yan-Kalinovskiy/team-23@3b9d8ddc0803e0353c66d8e347abb5cf93a66790 🚀

--- index.192b8b9f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t;e=document.querySelector("[data-menu-btn]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is__open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is__open")}))}();
-//# sourceMappingURL=index.192b8b9f.js.map
--- index.27f48297.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-btn]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is__open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is__open")}))})();
-//# sourceMappingURL=index.27f48297.js.map
