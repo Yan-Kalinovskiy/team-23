c17e115ead47d53ea2e47d3460628a59ce10e033
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2022-11-20 20:17:48 +0100

    Доповнено скріпти для модалок (для закриття вікон при переході за кнопкою-посиланням)

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -4,13 +4,19 @@
     openModalBuyBtn: document.querySelector("[data-modal-buy-open]"),
     closeModalBuyBtn: document.querySelector("[data-modal-buy-close]"),
     modalBuy: document.querySelector("[data-modal-buy]"),
+    modalBuyLink: document.querySelector("[data-modal-buy-link]"),
   };
 
   refs.openModalBuyBtn.addEventListener("click", toggleModalBuy);
   refs.closeModalBuyBtn.addEventListener("click", toggleModalBuy);
+  refs.modalBuyLink.addEventListener("click", removeModalBuy);
 
   function toggleModalBuy() {
     refs.modalBuy.classList.toggle("is-hidden");
+  }
+
+  function removeModalBuy() {
+    refs.modalBuy.classList.add("is-hidden");
   }
 })();
 
@@ -20,13 +26,19 @@
     openModalAddBtn: document.querySelector("[data-modal-add-open]"),
     closeModalAddBtn: document.querySelector("[data-modal-add-close]"),
     modalAdd: document.querySelector("[data-modal-add]"),
+    modalAddLink: document.querySelector("[data-modal-add-link]"),
   };
 
   refs.openModalAddBtn.addEventListener("click", toggleModalAdd);
   refs.closeModalAddBtn.addEventListener("click", toggleModalAdd);
+  refs.modalAddLink.addEventListener("click", removeModalAdd);
 
   function toggleModalAdd() {
     refs.modalAdd.classList.toggle("is-hidden");
+  }
+
+  function removeModalAdd() {
+    refs.modalAdd.classList.add("is-hidden");
   }
 })();
 
@@ -36,12 +48,18 @@
     openModalContactsBtn: document.querySelector("[data-modal-contacts-open]"),
     closeModalContactsBtn: document.querySelector("[data-modal-contacts-close]"),
     modalContacts: document.querySelector("[data-modal-contacts]"),
+    modalContactsLink: document.querySelector("[data-modal-contacts-link]"),
   };
 
   refs.openModalContactsBtn.addEventListener("click", toggleModalContacts);
   refs.closeModalContactsBtn.addEventListener("click", toggleModalContacts);
+  refs.modalContactsLink.addEventListener("click", removeModalContacts);
 
   function toggleModalContacts() {
     refs.modalContacts.classList.toggle("is-hidden");
+  }
+
+  function removeModalContacts() {
+    refs.modalContacts.classList.add("is-hidden");
   }
 })();
