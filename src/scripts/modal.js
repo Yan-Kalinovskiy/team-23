96f8ae238763ad70792863e778103fe180c2bdfe
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2022-11-23 03:31:12 +0100

    Під'єднано модальне вікно до contacts.html

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,15 +1,29 @@
-// Для модального вікна, яке викликається кнопкою "buy-it-now" на сторінці take-order
 (() => {
   const refs = {
     openModalBuyBtn: document.querySelector("[data-modal-buy-open]"),
     closeModalBuyBtn: document.querySelector("[data-modal-buy-close]"),
     modalBuy: document.querySelector("[data-modal-buy]"),
     modalBuyLink: document.querySelector("[data-modal-buy-link]"),
+    openModalAddBtn: document.querySelector("[data-modal-add-open]"),
+    closeModalAddBtn: document.querySelector("[data-modal-add-close]"),
+    modalAdd: document.querySelector("[data-modal-add]"),
+    modalAddLink: document.querySelector("[data-modal-add-link]"),
+    openModalContactsBtn: document.querySelector("[data-modal-contacts-open]"),
+    closeModalContactsBtn: document.querySelector("[data-modal-contacts-close]"),
+    modalContacts: document.querySelector("[data-modal-contacts]"),
+    modalContactsLink: document.querySelector("[data-modal-contacts-link]"),
   };
 
   refs.openModalBuyBtn.addEventListener("click", toggleModalBuy);
   refs.closeModalBuyBtn.addEventListener("click", toggleModalBuy);
   refs.modalBuyLink.addEventListener("click", removeModalBuy);
+  refs.openModalAddBtn.addEventListener("click", toggleModalAdd);
+  refs.closeModalAddBtn.addEventListener("click", toggleModalAdd);
+  refs.modalAddLink.addEventListener("click", removeModalAdd);
+  refs.openModalContactsBtn.addEventListener("click", toggleModalContacts);
+  refs.closeModalContactsBtn.addEventListener("click", toggleModalContacts);
+  refs.modalContactsLink.addEventListener("click", removeModalContacts);
+
 
   function toggleModalBuy() {
     refs.modalBuy.classList.toggle("is-hidden");
@@ -18,20 +32,6 @@
   function removeModalBuy() {
     refs.modalBuy.classList.add("is-hidden");
   }
-})();
-
-// Для модального вікна, яке викликається кнопкою "add-to-cart" на сторінці take-order
-(() => {
-  const refs = {
-    openModalAddBtn: document.querySelector("[data-modal-add-open]"),
-    closeModalAddBtn: document.querySelector("[data-modal-add-close]"),
-    modalAdd: document.querySelector("[data-modal-add]"),
-    modalAddLink: document.querySelector("[data-modal-add-link]"),
-  };
-
-  refs.openModalAddBtn.addEventListener("click", toggleModalAdd);
-  refs.closeModalAddBtn.addEventListener("click", toggleModalAdd);
-  refs.modalAddLink.addEventListener("click", removeModalAdd);
 
   function toggleModalAdd() {
     refs.modalAdd.classList.toggle("is-hidden");
@@ -40,20 +40,6 @@
   function removeModalAdd() {
     refs.modalAdd.classList.add("is-hidden");
   }
-})();
-
-// Для модального вікна на сторінці contacts
-(() => {
-  const refs = {
-    openModalContactsBtn: document.querySelector("[data-modal-contacts-open]"),
-    closeModalContactsBtn: document.querySelector("[data-modal-contacts-close]"),
-    modalContacts: document.querySelector("[data-modal-contacts]"),
-    modalContactsLink: document.querySelector("[data-modal-contacts-link]"),
-  };
-
-  refs.openModalContactsBtn.addEventListener("click", toggleModalContacts);
-  refs.closeModalContactsBtn.addEventListener("click", toggleModalContacts);
-  refs.modalContactsLink.addEventListener("click", removeModalContacts);
 
   function toggleModalContacts() {
     refs.modalContacts.classList.toggle("is-hidden");
